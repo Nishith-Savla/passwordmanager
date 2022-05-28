e9be4afc63e1b62d7ea3d8133f235d8898a974c9
Nishith-Savla <nishithsavla005@gmail.com>
2022-05-28 17:19:52 +0530

    :art: Add show combinators

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:passwordmanager/constants.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
+import 'package:passwordmanager/constants.dart' show purpleMaterialColor;
 import 'package:passwordmanager/firebase/authentication.dart';
-import 'package:passwordmanager/routes_generator.dart';
+import 'package:passwordmanager/routes_generator.dart' show RoutesGenerator;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  await dotenv.load(fileName: ".env");
+  await dotenv.load();
   final auth = Authentication();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
